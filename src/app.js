35bc491ae37f4f42d68c03943ae34fdd7e21229e
Nancy Acevedo <alepietri00@gmail.com>
2025-05-16 19:07:33 +0000

    Adj to Adjective .js fixed

--- src/app.js
+++ src/app.js
@@ -9,13 +9,13 @@ window.onload = function () {
   console.log("Hello Rigo from the console!");
 
   let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
+  let adjective = ["great", "big"];
   let noun = ["jogger", "racoon"];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        console.log(pronoun[i] + adjective[j] + noun[k] + ".com");
       }
     }
   }
